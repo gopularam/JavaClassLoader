f13da56daa3ef5dcf8415b37bd0347365c53611c
Bhanu Prakash Gopularam <bhanu.gopularam@yahoo.co.in>
2018-03-02 18:18:02 +0530

    added Panda machine learning notes, sample-data

--- AWS-apps/kids-awesome/functions/addusers/index.js
+++ AWS-apps/kids-awesome/functions/addusers/index.js
@@ -4,31 +4,64 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
 exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e);
 
-   var params = {
-    Item: {
-      userid:e.userid,
-      joindate: e.joindate,
-      message:e.message
-    },
-    TableName: 'kidsapp_users'
-  };  
-  /*var params = {
-    Item: {
-      userid:"Bhanu",
-      joindate:8080,
-      message: "Hello"
-    },
-    TableName: 'kidsapp_users'
-  };
- */
+  console.log('processing event: %j', e); 
+  console.log('processing operation: %j', e.userop); 
 
-  docClient.put(params, function(err, data) {
-    if(err) {
-      cb(err, 'null');
-    } else {
-      cb(null,data);
-    }
-  });
+  //ADD User
+  if(e.userop=="add") {
+    var params = {
+      Item: {
+        userid:e.userid,
+        joindate: e.joindate,
+        message:e.message
+      },
+      TableName: 'kidsapp_users'
+    };  
+    docClient.put(params, function(err, data) {
+      if(err) {
+        cb(err, 'null');
+      } else {
+        cb(null,data);
+      }
+    });
+  } else if (e.userop=="remove") {
+    //Remove User
+    var params = {
+      TableName: 'kidsapp_users',
+      Key:{
+          "userid":e.userid,
+          }
+      };
+    console.log("Attempting a conditional delete...");
+    docClient.delete(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete item. Error JSON:"+err);
+            cb(err, 'null');
+        } else {
+            console.log("DeleteItem succeeded:");
+            cb(null, data);
+        }
+      });
+  
+  }  else if (e.userop=="update") {
+    // Update User
+    var params = {
+      TableName: 'kidsapp_users',
+      Key:{
+          "userid":'222',
+          }
+      };
+    docClient.delete(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete item. Error JSON");
+            cb(err, 'null');
+        } else {
+            console.log("DeleteItem succeeded:");
+            cb(null, data);
+        }
+      });
+  } else {
+    console.log("Invalid Request:"+e.userop);
+  }
 }
